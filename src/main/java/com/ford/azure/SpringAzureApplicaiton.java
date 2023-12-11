930f6f87ac4fa5558d10415f73539fb0b50d63f8
Arunkumar0011 <arunbinarycode@gmail.com>
2023-12-11 17:18:03 +0530

    First Commit

---
+++ src/main/java/com/ford/azure/SpringAzureApplicaiton.java
@@ -0,0 +1,13 @@
+package com.ford.azure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAzureApplicaiton {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAzureApplicaiton.class, args);
+	}
+
+}
